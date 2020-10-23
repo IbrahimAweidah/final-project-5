41b587509018dc73bbf2a5ae9a4d3ae1b617ee98
IbrahimA <ibrahim.aweidah@carpetvista.se>
2020-10-24 00:47:17 +0200

    Update implementation

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,41 @@
 import 'source-map-support/register'
-
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+const itemId = uuid.v4()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  const updatedItem = {
+    id: itemId,
+    ...updatedTodo
+  }
+
+  var params = {
+    TableName: todoTable,
+    Key: { todoId },
+    UpdateExpression: 'set name = :newname',
+    ExpressionAttributeValues: { ':newname': updatedItem.name }, //TODO: add done and dueDate
+  }
+  try {
+    await docClient.update(params).promise()
+  } catch (err) {
+    return err
+  }
+
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: "Updated"
+  }
 }
