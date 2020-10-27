75c8d0ac470b9aa041212f53a0f3cff9767c1b54
IbrahimA <ibrahim.aweidah@carpetvista.se>
2020-10-27 13:47:10 +0100

    Latest auth tests

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,28 +1,21 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
-import 'source-map-support/register'
-import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger' // winston logger
-import { Jwt } from '../../auth/Jwt' // interface of JWT
-import { JwtPayload } from '../../auth/JwtPayload' // return type of verified token
-import Axios from 'axios'
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda';
+import 'source-map-support/register';
+import { verify, decode} from 'jsonwebtoken';
+import { createLogger } from '../../utils/logger';
+import Axios from 'axios';
+import { JwtPayload } from '../../auth/JwtPayload';
+import * as util from 'util';
+import {Jwt} from '../../auth/Jwt'
+const logger = createLogger('auth');
 
-const logger = createLogger('auth')
-
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://dev-0rw8r0s2.eu.auth0.com/.well-known/jwks.json'
-
+const jwksUrl = 'https://dev-u6fy-qek.us.auth0.com/.well-known/jwks.json';
 export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
-
+  logger.info('Authorizing a user', event.authorizationToken);
   try {
-    // verified passed in token from event Auth Header
-    const jwtToken = await verifyToken(event.authorizationToken)
-    logger.info('User was authorized', jwtToken)
-
+    const jwtToken = await verifyToken(event.authorizationToken);
+    logger.info('User was authorized', jwtToken);
     return {
-      principalId: jwtToken.sub, // user id
+      principalId: jwtToken.sub,
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -33,10 +26,9 @@ export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAutho
           }
         ]
       }
-    }
+    };
   } catch (e) {
-    logger.error('User not authorized', { error: e.message })
-
+    logger.error('User not authorized', { error: e.message });
     return {
       principalId: 'user',
       policyDocument: {
@@ -49,101 +41,37 @@ export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAutho
           }
         ]
       }
-    }
+    };
   }
 }
-
-// verify token with event.authorizationToken:
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-
-  // 2. Extract the JWT from the request's authorization header.
-  const token = getToken(authHeader) // get JWT token from event.authorizationToken
+  const token = getToken(authHeader)
+  const response = await Axios.get(jwksUrl);
+  const jwks = response.data;
+  const keys:any[] = jwks.keys;
+  logger.info("jwks - "+util.inspect(jwks, false, null, true));
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
-  if (!jwt) {
-    logger.error('Invalid token')
-    throw new Error('401 error invalid token')
-  }
-
-  const { header } = jwt;
-
-  // 3. Decode the JWT and grab the kid property from the header.
-  let key = await getSigningKey(jwksUrl, header.kid)
-
-  // verify with token and certificate
-  return verify(token, key.publicKey, { algorithms: ['RS256'] }) as JwtPayload
-}
-
-
-function getToken(authHeader: string): string {
-  if (!authHeader) throw new Error('No authentication header')
-
-  if (!authHeader.toLowerCase().startsWith('bearer '))
-    throw new Error('Invalid authentication header')
-
-  const split = authHeader.split(' ')
-  const token = split[1]
-
-  return token
-}
-
-// 3. Decode the JWT and grab the kid property from the header.
-const getSigningKey = async (jwkurl, kid) => {
-
-  // 1. Retrieve the JWKS and filter for potential signature verification keys.
-  let res = await Axios.get(jwkurl, {
-    // set Headers to allow CORS: 
-    headers: {
-      'Content-Type': 'application/json',
-      "Access-Control-Allow-Origin": "*",
-      'Access-Control-Allow-Credentials': true,
-    }
-  });
-
-  // JSON Web Key object has 'keys' property which is an array of JWKs:
-  let keys  = res.data.keys;
-
-  // 4. Find the signature verification key in the filtered JWKS with a matching kid property: 
-  const signingKeys = keys
-    .filter(key =>
-        key.alg === 'RS256' // has algorithm as RS256s
-        && key.kty === 'RSA' // supporting RSA key type
-        && key.use === 'sig' // JWK property `use` determines the JWK is for signing
-        && key.kid  // has 'kid' for unique key id
-        // has x5c property for public certificate
-        && ((key.x5c && key.x5c.length)) 
-    )
-    .map(key => {
-      // return key with properties: kid, nbf and publicKey
-      return { 
-        kid: key.kid, // unique key id
-        nbf: key.nbf, // this not be logged?
-        publicKey: certToPEM(key.x5c[0]) // certificate
-      };
+  const signingKeys = keys.filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
+      && key.kty === 'RSA' // We are only supporting RSA
+      && key.kid           // The `kid` must be present to be useful for later
+      && key.x5c && key.x5c.length // Has useful public keys (we aren't using n or e)
+    ).map(key => {
+      return { kid: key.kid, nbf: key.nbf, x5c: key.x5c[0] };
     });
-
-  if (!signingKeys.length) {
-    logger.error("No signing keys found")
-    throw new Error('Invalid signing keys')
-  }
-
-  // Finding the exact signature verification key: 
-  const signingKey = signingKeys.find(key => key.kid === kid); 
-
-  if (!signingKey || !signingKeys.length) {
-    logger.error("No signing keys found")
-    throw new Error('Invalid signing keys')
-  }
-
-  logger.info("Signing keys created successfully ", signingKey)
-
-  return signingKey
-};
-
-// 5. Using the x5c property build a certificate which will be used to verify the JWT signature. 
-const certToPEM = (cert) => {
-  // cert = cert.match(/.{1,64}/g).join('\n');
-  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
-
-  return cert;
+const signingKey = signingKeys.find(key => key.kid === jwt.header.kid);
+let certValue:string = signingKey.x5c;
+  
+  certValue = certValue.match(/.{1,64}/g).join('\n');
+  const finalCertKey:string = `-----BEGIN CERTIFICATE-----\n${certValue}\n-----END CERTIFICATE-----\n`;
+  logger.info("finalCertKey - "+util.inspect(finalCertKey, false, null, true));
+  let jwtPayload:JwtPayload = verify(token, finalCertKey, { algorithms: ['RS256'] }) as JwtPayload; 
+  return jwtPayload;
+}
+function getToken(authHeader: string): string {
+  if (!authHeader) throw new Error('No authentication header');
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header');
+  const split = authHeader.split(' ');
+  const token = split[1];
+  return token;
 }
